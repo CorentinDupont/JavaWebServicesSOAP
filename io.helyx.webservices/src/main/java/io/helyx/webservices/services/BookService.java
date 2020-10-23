b9c11f2278ab3af59e04b552c1e04b6c2f397837
Corentin Dupont <corentin@helyx.io>
2020-10-23 14:58:52 +0200

    🚧 feat(services) : create author and book base services

---
+++ io.helyx.webservices/src/main/java/io/helyx/webservices/services/BookService.java
@@ -0,0 +1,32 @@
+package io.helyx.webservices.services;
+
+import io.helyx.webservices.models.Book;
+
+public class BookService implements ICrudService<Book> {
+
+	public Book readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Book create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Book read(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Book update(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
